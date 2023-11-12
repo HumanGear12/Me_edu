f154a348d2feff518e1bffadce80daae9fc1370f
atguigu <3211699845@qq.com>
2023-11-12 09:34:47 +0800

    test no druid

--- edu_realtame/src/main/java/com/atguigu/edu/realtime/util/DruidTest.java
+++ edu_realtame/src/main/java/com/atguigu/edu/realtime/util/DruidTest.java
@@ -2,9 +2,10 @@ package com.atguigu.edu.realtime.util;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidPooledConnection;
+import com.atguigu.edu.realtime.common.EduConfig;
 import org.junit.Test;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DruidTest {
 
@@ -16,6 +17,44 @@ public class DruidTest {
         DruidPooledConnection connection = druidDataSource.getConnection();
 
         System.out.println(connection);
+    }
+
+    @Test
+    public void test2(){
+        try {
+            // Register the Phoenix JDBC driver
+            Class.forName(EduConfig.PHOENIX_DRIVER);
+
+            // Create a connection to Phoenix
+            Connection connection = DriverManager.getConnection(EduConfig.PHOENIX_SERVER);
+
+            // Create a statement
+            Statement statement = connection.createStatement();
+
+            // Execute a query
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM SYSTEM.TASK");
+
+            // Process the result set
+            while (resultSet.next()) {
+                // Handle each row
+                String column1Value = resultSet.getString("column1");
+                String column2Value = resultSet.getString("column2");
+                // Add more columns as needed
+                System.out.println("Column1: " + column1Value + ", Column2: " + column2Value);
+            }
+
+            // Close resources
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void test3(){
+        
     }
 
     }
