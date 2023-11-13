ebef48058301779897e1b607138f7c22eef91420
atguigu <3211699845@qq.com>
2023-11-13 08:46:10 +0800

    test phoenix

---
+++ PhoenixDemo/src/main/java/com/atguigu/phoenix/ClientDemo.java
@@ -0,0 +1,25 @@
+package com.atguigu.phoenix;
+mport java.sql.*;
+
+/**
+ * Created by Smexy on 2023/11/7
+ */
+public class ClientDemo
+{
+    public static void main(String[] args) throws SQLException {
+        // 1.添加链接
+        String url = "jdbc:phoenix:192.168.12.122:2181";
+        // 2.获取连接
+        Connection connection = DriverManager.getConnection(url);
+        // 3.编译SQL语句
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from student");
+        // 4.执行语句
+        ResultSet resultSet = preparedStatement.executeQuery();
+        // 5.输出结果
+        while (resultSet.next()){
+            System.out.println(resultSet.getString(1) + ":" + resultSet.getString(2) + ":" + resultSet.getString(3));
+        }
+        // 6.关闭资源
+        connection.close();
+    }
+}
